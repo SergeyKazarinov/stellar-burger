9395f3f5450436caf6e7aefbb4fc99b642b67ea7
Sergey Kazarinov <kazarinov092@gmail.com>
2023-01-09 07:26:27 +0500

    feat(aut

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import login from './Login.module.scss';
+import register from './Register.module.scss';
 import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import { RouteComponentProps, withRouter } from "react-router";
 
-const Login = ({history}: RouteComponentProps): JSX.Element => {
-  const [value, setValue] = React.useState('');
+const Register = ({history}: RouteComponentProps): JSX.Element => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isVisiblePassword, setIsVisiblePassword] = useState(false);
 
@@ -12,20 +13,31 @@ const Login = ({history}: RouteComponentProps): JSX.Element => {
     setIsVisiblePassword(!isVisiblePassword)
   }
 
-  const handleMoveToRegister = () => {
-    history.push('/register')
+  const handleMoveToLogin = () => {
+    history.push('/login')
   }
 
   return (
-    <section className={login.login}>
-      <div className={login.container}>
-        <h2 className={`text text_type_main-large ${login.title}`}>Вход</h2>
-        <form className={login.form}>
+    <section className={register.register}>
+      <div className={register.container}>
+        <h2 className={`text text_type_main-large ${register.title}`}>Регистрация</h2>
+        <form className={register.form}>
+          <Input
+            type={'text'}
+            placeholder={'Имя'}
+            onChange={e => setName(e.target.value)}
+            value={name}
+            name={'name'}
+            error={false}
+            errorText={'Ошибка'}
+            size={'default'}
+            extraClass="mt-6"
+          />
           <Input
             type={'text'}
             placeholder={'E-mail'}
-            onChange={e => setValue(e.target.value)}
-            value={value}
+            onChange={e => setEmail(e.target.value)}
+            value={email}
             name={'email'}
             error={false}
             errorText={'Ошибка'}
@@ -50,19 +62,13 @@ const Login = ({history}: RouteComponentProps): JSX.Element => {
             type="primary"
             extraClass="mt-6"
           >
-            Войти
+            Зарегистрироваться
           </Button>
         </form>
         <p className={`mt-20 text text_type_main-default text_color_inactive`}>
-          Вы — новый пользователь?&#8194;
-          <Button htmlType="button" type="secondary" size="medium" extraClass={login.button} onClick={handleMoveToRegister}>
-            Зарегистрироваться
-          </Button>
-        </p>
-        <p className={`mt-4 text text_type_main-default text_color_inactive`}>
-          Забыли пароль?&#8194;
-          <Button htmlType="button" type="secondary" size="medium" extraClass={login.button}>
-            Восстановить пароль
+          Уже зарегистрированы?&#8194;
+          <Button htmlType="button" type="secondary" size="medium" extraClass={register.button} onClick={handleMoveToLogin}>
+            Войти
           </Button>
         </p>
       </div>
@@ -70,4 +76,4 @@ const Login = ({history}: RouteComponentProps): JSX.Element => {
   );
 }
 
-export default withRouter(Login);
+export default withRouter(Register);
